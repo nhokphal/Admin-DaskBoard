ccf099123dd689c43a9467769e0a1f9bd51d67c0
nhokphal <phalnhok@gmail.com>
2022-10-01 12:00:05 +0700

    add

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components'
+import { SideBar } from '../../Components/SideBar';
+
+
+const Container = styled.div
+`
+  display: flex;
+  width: 120vh;
+  height: 60vw;
+`;
+const Wrapper = styled.div
+`
+  flex:7;
+`;
+
+
+const Title = styled.h2  
+` 
+`;  
+
+
+
+export const Home = () => {
+  return (
+    <Container>
+      <SideBar />
+      <Wrapper>
+
+        container
+      </Wrapper>
+     
+      </Container>
+ 
+  )
+}
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+
+
+
+export const Login = () => {
+  return (
+    <div>
+        
+        <Link to="/login">Login</Link>
+    </div>
+  )
+}
